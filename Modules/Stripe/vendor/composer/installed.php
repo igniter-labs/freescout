41fe85692d2564c5bc4017f85aa93abdca9daa78
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-05-17 23:37:03 +0100

    wip

---
+++ Modules/Stripe/vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wedevs_official/laravel_freescout_stripe_module',
+        'pretty_version' => '1.0.2',
+        'version' => '1.0.2.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'laravel/framework' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v10.4.0',
+            'version' => '10.4.0.0',
+            'reference' => '87def896d9a2e2914871408277d67432cb112f71',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wedevs_official/laravel_freescout_stripe_module' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
